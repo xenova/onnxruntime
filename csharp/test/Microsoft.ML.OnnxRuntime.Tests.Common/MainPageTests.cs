09c8d192e2ac7a67540d52754d7b56164d426060
carzh <wolfivyaura@gmail.com>
2024-11-07 10:13:28 -0800

    started mainpage tests + added appium driver to common

---
+++ csharp/test/Microsoft.ML.OnnxRuntime.Tests.Common/MainPageTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+using Xunit;
+
+namespace Microsoft.ML.OnnxRuntime.Tests
+{
+    public class MainPageTests
+    {
+        protected AppiumDriver App => AppiumSetup.App;
+
+        [Fact]
+        public void ClickRunAllTest()
+        {
+            var element = App.FindElement(By.XPath(".//*[text()='Run All  ►►']"));
+
+            Assert.Equal(element.Text, "Run All  ►►");
+
+            element.Click();
+
+            Task.Delay(600).Wait();
+        }
+    }
+}
