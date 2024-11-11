01c4d72562c83ce0241ce4cad5db87b02c044a31
carzh <wolfivyaura@gmail.com>
2024-11-11 11:35:57 -0800

    started adding windows driver

--- csharp/test/Microsoft.ML.OnnxRuntime.Tests.Common/MainPageTests.cs
+++ csharp/test/Microsoft.ML.OnnxRuntime.Tests.Common/MainPageTests.cs
@@ -9,6 +9,17 @@ using OpenQA.Selenium.Appium.Windows;
 using Xunit;
 
 namespace Microsoft.ML.OnnxRuntime.Tests;
+// Add a CollectionDefinition together with a ICollectionFixture
+// to ensure that setting up the Appium server only runs once
+// xUnit does not have a built-in concept of a fixture that only runs once for the whole test set.
+[CollectionDefinition("UITests")]
+public sealed class UITestsCollectionDefinition : ICollectionFixture<AppiumSetup>
+{
+
+}
+
+// Add all tests to the same collection as above so that the Appium server is only setup once
+[Collection("UITests")]
 public class MainPageTests
 {
     protected AppiumDriver App => AppiumSetup.App;
@@ -22,11 +33,11 @@ public class MainPageTests
 
         element.Click();
 
-        String color = "SuccessfulTestsColor";
-        String binding = "Passed";
-        MobileElement label = driver.findElementByXPath("//Label[@TextColor='" + color + "' and @Text='" + binding + "']");
-        String labelText = label.getText();
-        System.out.println("The text displayed by the Label is: " + labelText);
+        //String color = "SuccessfulTestsColor";
+        //String binding = "Passed";
+        //MobileElement label = driver.findElementByXPath("//Label[@TextColor='" + color + "' and @Text='" + binding + "']");
+        //String labelText = label.getText();
+        //System.out.println("The text displayed by the Label is: " + labelText);
 
         Task.Delay(600).Wait();
     }
