49ffd6181292c42eb1d5229bb4fb59678fb37cb3
carzh <wolfivyaura@gmail.com>
2024-11-13 10:27:55 -0800

    attempted to create device-specific folder but ran into the same error

--- csharp/test/Microsoft.ML.OnnxRuntime.Tests.Common/MainPageTests.cs
+++ csharp/test/Microsoft.ML.OnnxRuntime.Tests.Common/MainPageTests.cs
@@ -12,17 +12,31 @@ namespace Microsoft.ML.OnnxRuntime.Tests;
 // Add a CollectionDefinition together with a ICollectionFixture
 // to ensure that setting up the Appium server only runs once
 // xUnit does not have a built-in concept of a fixture that only runs once for the whole test set.
-[CollectionDefinition("UITests")]
+[CollectionDefinition("Microsoft.ML.OnnxRuntime.Tests")]
 public sealed class UITestsCollectionDefinition : ICollectionFixture<AppiumSetup>
 {
 
 }
 
 // Add all tests to the same collection as above so that the Appium server is only setup once
-[Collection("UITests")]
+[Collection("Microsoft.ML.OnnxRuntime.Tests")]
 public class MainPageTests
 {
     protected AppiumDriver App => AppiumSetup.App;
+
+    [Fact]
+    public void FailingOutputTest()
+    {
+        Console.WriteLine("This is test output.");
+        throw new Exception("This is meant to fail.");
+    }
+
+    [Fact]
+    public void SuccessfulTest()
+    {
+        Assert.True(true);
+    }
+
 
     [Fact]
     public void ClickRunAllTest()
