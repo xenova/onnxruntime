801b7327196eb64e35fd82ac1daf5ff65ba0c44b
carzh <wolfivyaura@gmail.com>
2024-12-26 15:46:35 -0800

    updates to try to get android project working

--- csharp/test/Microsoft.ML.OnnxRuntime.Tests.UITest/Platforms/Android/AppiumSetup.cs
+++ csharp/test/Microsoft.ML.OnnxRuntime.Tests.UITest/Platforms/Android/AppiumSetup.cs
@@ -21,18 +21,20 @@ public partial class AppiumSetup
 
         var androidOptions = new AppiumOptions
         {
+            // If running on local, uncomment the below line to use with the specified emulator
+            DeviceName = "emulator-5554",
             // Specify windows as the driver, typically don't need to change this
             AutomationName = "UIAutomator2",
             // Always Windows for Windows
             PlatformName = "Android",
             //PlatformVersion = "17.0",
             //// The identifier of the deployed application to test
-            //App = "ORT.CSharp.Tests.MAUI",
+            App = "C:\\Users\\carolinezhu\\Documents\\cowboy-coding\\onnxruntime\\csharp\\test\\Microsoft.ML.OnnxRuntime.Tests.MAUI\\bin\\Debug\\net8.0-android\\ORT.CSharp.Tests.MAUI-Signed.apk",
         };
 
         // Note there are many more options that you can use to influence the app under test according to your needs
 
-        driver = new AndroidDriver(androidOptions);
+        driver = new AndroidDriver(new Uri("http://127.0.0.1:4723/wd/hub"), androidOptions);
     }
 
     public void Dispose()
